749b65831061b74ac608b9b8e8687613b794f3cc
Sam <84884629+axiomly@users.noreply.github.com>
2026-03-11 16:42:06 -0300

    chore(api): add Prisma service and database module

---
+++ apps/api/src/database/prisma.service.ts
@@ -0,0 +1,23 @@
+import { OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from 'src/generated/prisma/client';
+
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL as string,
+    });
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await super.$connect();
+  }
+
+  async onModuleDestroy() {
+    await super.$disconnect();
+  }
+}
